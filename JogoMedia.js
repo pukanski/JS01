5ef661e165133a601ad0904bcf1148cec6edc3b9
Gustavo Pukanski <pukanski@hotmail.com>
2025-06-27 21:37:10 -0300

    Atividade 1 JavaScript

---
+++ JogoMedia.js
@@ -0,0 +1,13 @@
+alert("Bem vindo ao jogo da média");
+let qtdNum = parseInt(prompt("Quantos números você gostaria de usar para calcular a média?"));
+
+let soma = 0;
+
+for (let i = 1; i <= qtdNum; i++) {
+  let num = parseInt(prompt(`Digite o ${i}º número:`));
+  soma += num;
+}
+
+let media = soma / qtdNum;
+
+alert(`A média dos ${qtdNum} números é igual a ${media}`);
